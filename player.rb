ec8a174ba32a03ee5071b19edb206ccc2434cc37
Max Anselm <silverhammermba+github@gmail.com>
2012-03-14 22:25:49 -0400

    Barely working alpha. Players 1 and 2 can move around with joysticks

--- player.rb
+++ player.rb
@@ -6,49 +6,53 @@ class Player
 		@ch = character # character that the worm is made of
 		@color = color_number # Curses color pair number
 		@start_pos = start_pos # starting position
-		@start_dir = dir # starting direction (method)
+		@start_dir = direction # starting direction (method)
 		@ctrl = controller # Xbox controller
-		@wins = 0 # score
+		@score = 0
+		@bind = {} # key binding hash
 		reset
 	end
 
 	def reset
-		@worm = [@start_pos] # reset start position
+		@pos = [@start_pos] # reset start position
 		@last = [] # TODO hacky
 		send(@start_dir) # reset start direction
 		@last = @dir.dup # TODO hacky
-		@fail = false # player has not crashed yet
+		@crashed = false # player has not crashed yet
 		@ready = false # player is not ready yet
+		@i = false # for keeping track of when to eat the tail
 	end
 
-	def eat_tail i
-		return if i % 2 == 1
-		if @worm.length > 10 or (@fail and @worm.length > 1)
-			Curses.stdscr.print *@worm.shift, ' '
+	def eat_tail
+		# remove tail segments
+		return if @i = !@i # eat the tail every other iteration
+		if @pos.length > 10 or (@crashed and @pos.length > 1)
+			Curses.stdscr.print *@pos.shift, ' '
 		end
 	end
 
-	def print rev = true
-		return if @fail
-		Curses.stdscr.attron((rev ? 0 : Curses.color_pair(@color)) | Curses::A_BOLD) do
-			Curses.stdscr.print *@worm[-1], @ch
+	def print head = false
+		# print a section of the snake
+		Curses.stdscr.attron((head ? 0 : Curses.color_pair(@color)) | Curses::A_BOLD) do
+			Curses.stdscr.print *@pos[-1], @ch
 		end
-		@last = @dir
+		@last = @dir # a new segment has been drawn, so start accepting new directions
 	end
 
 	def next
-		return nil if @fail
-		[@worm[-1][0] + @dir[0], @worm[-1][1] + @dir[1]]
+		# generate a new segment
+		[@pos[-1][0] + @dir[0], @pos[-1][1] + @dir[1]]
 	end
 
 	def call ch
+		# call a key binding
 		send(@bind[ch]) if @bind[ch]
 	end
 
-	def joy
+	def joystick
 		# deadzone
-		if @controller.axis[0] and @controller.axis[1] and @controller.axis[0] ** 2 + @controller.axis[1] ** 2 > 169000000
-			case Math.atan2(@controller.axis[1], @controller.axis[0])
+		if @ctrl.axis[0] and @ctrl.axis[1] and @ctrl.axis[0] ** 2 + @ctrl.axis[1] ** 2 > 169000000
+			case Math.atan2(@ctrl.axis[1], @ctrl.axis[0])
 			when (-Math::PI/4)..(Math::PI/4)
 				right
 			when (Math::PI/4)..(Math::PI*3/4)
@@ -61,6 +65,7 @@ class Player
 		end
 	end
 
+	# movement methods
 	def up
 		@dir = [-1, 0] unless @last[0] == 1
 	end
@@ -78,22 +83,22 @@ class Player
 	end
 
 	def move
-		return if @fail
-		print false
-		@worm << self.next
-		print
+		# move the worm forward
+		unless @crashed
+			print
+			@pos << self.next
+			print true
+		end
+		eat_tail
 	end
 
 	def explode
-		return if @fail
-		@fail = true
-		return Explosion.new @worm[-1]
+		@crashed = true
+		#return Explosion.new @worm[-1]
 	end
 
-	def check_collision
-		return if @fail
-		return Curses.stdscr[*self.next] != 32
+	def can_move?
+		Curses.stdscr[*self.next] == 32
 	end
-
 end
 
--- test.rb
+++ test.rb
@@ -2,19 +2,38 @@
 
 require 'curses'
 require 'xbox'
-require 'player'
+require './player'
 
 num_players = ARGV.shift.to_i
 
+# find all joysticks
 con = Dir.entries('/dev/input').reject { |dev| dev !~ /^js/ }.map { |dev| Xbox360Controller.new("/dev/input/" + dev) }
 
-player = []
+# we need the stdscreen to set player positions
+stdscr = Curses.init
 
+# set up Curses
+Curses.echo = false
+Curses.curs_set 0
+Curses.ESCDELAY = 0
+
+Curses.start_color
+Curses.init_pair 1, 1, 0 # player colors...
+Curses.init_pair 2, 5, 0
+Curses.init_pair 3, 4, 0
+Curses.init_pair 4, 2, 0
+Curses.init_pair 5, 6, 3 # walls
+Curses.init_pair 6, 3, 0 # explosions
+
+stdscr.keypad = true
+
+# initialize players
+player = []
 if num_players > 0
-	player[0] = Player.new("Player 1", "#", 0, [scr.lines / 2, 1], :right, con[0])
+	player[0] = Player.new("Player 1", "#", 1, [stdscr.lines / 2, 1], :right, con[0])
 end
 if num_players > 1
-	player[1] = Player.new("Player 2", "@", 1, [scr.lines / 2, scr.columns - 2], :left, con[1])
+	player[1] = Player.new("Player 2", "@", 2, [stdscr.lines / 2, stdscr.columns - 2], :left, con[1])
 end
 if num_players > 2
 	#player[2] = Player.new("Player 3", "%", 2, :right, con[2])
@@ -23,45 +42,51 @@ if num_players > 3
 	#player[3] = Player.new("Player 4", "&", 3, :right, con[3])
 end
 
-
+# set up input threads
 input_threads = []
-
-Curses.init do |scr|
-	Curses.echo = false
-	Curses.curs_set 0
-	Curses.ESCDELAY = 0
-	scr.keypad = true
-
-	con.each_with_index do |controller, i|
-		input_threads << Thread.new(controller, i) do |c, j|
-			while e = c.event
-				scr.pos = j, 0
-				scr.clrtoeol
-				scr.print(j, 0, c.axis.inspect + ' ' + c.button.inspect)
-				scr.refresh
-				Thread.current.exit if e.type == :button and e.number == 7 and e.value == 1
+con.each_with_index do |controller, i|
+	input_threads << Thread.new(controller, i) do |c, j|
+		while e = c.event
+			if e.type == :axis and e.number <= 1
+				player[j].joystick
 			end
+			stdscr.pos = j, 0
+			stdscr.clrtoeol
+			stdscr.print(j, 0, c.axis.inspect + ' ' + c.button.inspect)
+			stdscr.refresh
+			#Thread.current.exit if e.type == :button and e.number == 7 and e.value == 1
 		end
 	end
-
-	input_threads << Thread.new do
-		while ch = scr.getc
-			scr.print(con.length, 0, ch.inspect)
-			scr.refresh
-			Thread.current.exit if ch == 27
-		end
-	end
-
-	game = Thread.new do
-		i = 0
-		while true
-			sleep 1
-			scr.print(con.length + 1, 0, i = (i + 1) % 10)
-			scr.refresh
-		end
-	end
-
-	input_threads.each { |thread| thread.join }
-
-	con.each { |controller| controller.close }
 end
+# keyboard input
+keyboard = Thread.new do
+	while ch = stdscr.getc
+		if ch == 27
+			input_threads.each { |thread| thread.exit }
+			Thread.current.exit 
+		end
+	end
+end
+
+# game logic here
+game = Thread.new do
+	while true
+		sleep 0.1
+
+		player.each do |p|
+			p.move
+		end
+
+		stdscr.refresh
+	end
+end
+
+input_threads.each { |thread| thread.join } # why do I need TODO this?
+# exit when the keyboard closes (ESC pressed)
+keyboard.join
+
+# close all joystick devices
+con.each { |controller| controller.close }
+
+# and Curses
+Curses.close
